e3bb25c7d9044e71cfb73cba174ecb26f572e044
pyoor <pyoor@users.noreply.github.com>
2016-12-04 17:45:55 -0500

    Minor changes to config and import.

--- utils/config_import.py
+++ utils/config_import.py
@@ -2,13 +2,19 @@ import yaml
 
 
 def read_config(config_file, section):
+    sections = ['project', section]
+
     with open(config_file, 'r') as stream:
         cfg = yaml.load(stream)
 
+    config_data = {}
+
     try:
-        data = cfg[section]
+        for section in sections:
+            for k, v in cfg[section].iteritems():
+                config_data[k] = v
     except KeyError:
         print "Error:  Unable to find section %s" % section
         raise
 
-    return data
+    return config_data
